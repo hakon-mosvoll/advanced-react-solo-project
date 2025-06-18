b0155b930ca20606569c8a93490578a3c24c8f04
Håkon Mosvoll <hakon.mosvoll@spk.no>
2025-06-18 17:33:09 +0200

    fix: Simple adjustment of the position of the tooltip if it goes out of bounds

--- src/components/Tooltip/TooltipContent.jsx
+++ src/components/Tooltip/TooltipContent.jsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import { useId } from "react";
+import { useId, useLayoutEffect, useRef, useState } from "react";
 import { BsX } from "react-icons/bs";
 
 export default function TooltipContent({
@@ -11,15 +11,43 @@ export default function TooltipContent({
 	position = "top",
 	...rest
 }) {
-	const tooltipContentClasses = classNames("tooltip-content", className, {
-		"tooltip-content-open": open,
-		[`tooltip-content-${position}`]: position,
-	});
-
+	const tooltipRef = useRef(null);
+	const [adjustedPosition, setAdjustedPosition] = useState(position);
 	const titleId = useId();
 
+	useLayoutEffect(() => {
+		if (!tooltipRef.current) return;
+
+		if (!open) {
+			setAdjustedPosition(position);
+			return;
+		}
+
+		const rect = tooltipRef.current.getBoundingClientRect();
+		const { innerWidth, innerHeight } = window;
+
+		let newPosition = position;
+
+		if (position === "top" && rect.top < 0) {
+			newPosition = "bottom";
+		} else if (position === "bottom" && rect.bottom > innerHeight) {
+			newPosition = "top";
+		} else if (position === "left" && rect.left < 0) {
+			newPosition = "right";
+		} else if (position === "right" && rect.right > innerWidth) {
+			newPosition = "left";
+		}
+
+		setAdjustedPosition(newPosition);
+	}, [open, position]);
+
+	const tooltipContentClasses = classNames("tooltip-content", className, {
+		"tooltip-content-open": open,
+		[`tooltip-content-${adjustedPosition}`]: adjustedPosition,
+	});
+
 	return (
-		<div className={tooltipContentClasses} {...rest}>
+		<div ref={tooltipRef} className={tooltipContentClasses} {...rest}>
 			<strong
 				id={`tooltip-content-title-${titleId}`}
 				className="tooltip-content-title"
